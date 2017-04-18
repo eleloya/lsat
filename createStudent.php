104c6ba2c69ec2a5241c3c124fd170b0e42d9ad5
eleloya <ele.loya@gmail.com>
2017-04-18 18:18:22 -0500

    Added create student functionality

---
+++ createStudent.php
@@ -0,0 +1,72 @@
+<?php
+
+require 'core/init.php';
+
+$user = new User();
+$user->checkIsValidUser('teacher');
+
+?>
+
+<!doctype html>
+<html class="no-js" lang="en">
+<head>
+  <title>LSAT | Agregar Alumno a Grupo</title>
+  <?php include 'includes/templates/headTags.php' ?>
+</head>
+
+<body>
+
+  <?php include 'includes/templates/header.php' ?>
+
+  <section class="scroll-container" role="main">
+
+    <div class="row">
+      <?php include 'includes/templates/teacherSidebar.php' ?>  
+      <div class="large-9 medium-8 columns">
+        <h3>Grupo</h3>
+        <h4 class="subheader">Agregar nuevo alumno a grupo existente</h4>
+        <hr>  
+
+        <form> 
+          <div class="row"> 
+            <div class="large-4 columns"> 
+            <label>Nombre del grupo <input id="groupname" type="text" placeholder="TC-0001" /> </label> 
+            </div>
+          </div>
+          <div class="row"> 
+            <div class="large-12 columns"> 
+              <label>Alumnos<input id="students" type="text" placeholder="Matrículas de alumnos separadas por comas... A012345, A02389"/> </label>
+            </div> 
+          </div>  
+          <a href="#" onclick="createGroup()" class="button round small right">Crear</a>
+        </form>
+
+      </div>
+    </div>
+  </section>
+
+
+  <?php include 'includes/templates/footer.php' ?>
+
+  <script src="js/vendor/jquery.js"></script>
+  <script src="js/foundation.min.js"></script>
+
+	<script>
+	$(document).foundation();
+	function createGroup() {
+		var groupname = $("#groupname").val();
+		var students = $("#students").val();
+
+		$.post("controls/doAction.php", { action:"addStudentsToGroup", groupname: groupname, students: students})
+		.done(function(data) {
+			data = JSON.parse(data);
+			if (data.message == 'success') {
+				window.location.replace('./students.php');
+			} else {
+				alert("Error:\n\n" + data.message);
+			}
+		});
+	}
+	</script>
+</body>
+</html>
